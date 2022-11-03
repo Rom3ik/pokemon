38c61192ebeca2397adb1c5625327a635d606af6
Ramazan <rom_3ik@bk.ru>
2022-11-03 20:39:28 +0400

    pokemon details design

--- src/app/modules/pokemon-details/pokemon-details.component.ts
+++ src/app/modules/pokemon-details/pokemon-details.component.ts
@@ -1,7 +1,7 @@
-import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
+import {AfterViewInit, ChangeDetectionStrategy, Component, OnInit, Renderer2} from '@angular/core';
 import {ActivatedRoute, Router} from '@angular/router';
 import {PokemonService} from '../../core/services/pokemon.service';
-import {filter, map, share, shareReplay, tap} from 'rxjs/operators';
+import {map} from 'rxjs/operators';
 import {Observable} from 'rxjs';
 
 @Component({
@@ -10,19 +10,22 @@ import {Observable} from 'rxjs';
   styleUrls: ['./pokemon-details.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class PokemonDetailsComponent implements OnInit {
+export class PokemonDetailsComponent implements OnInit, AfterViewInit {
 
   pokemon: any;
+  pokemonStats: any[] = [];
   pokemonDescription$: Observable<string>;
 
   constructor(
     private router: Router,
     private activatedRoute: ActivatedRoute,
-    private pokemonService: PokemonService
+    private pokemonService: PokemonService,
+    private renderer2: Renderer2
   ) {
     this.activatedRoute.queryParams.subscribe(params => {
       if (this.router.getCurrentNavigation().extras.state) {
         this.pokemon = this.router.getCurrentNavigation().extras.state.pokemon;
+        this.initializePokemonStats(this.pokemon.stats);
         this.getPokemonSpecies(this.pokemon.id);
       } else {
         this.router.navigateByUrl('/tabs', {replaceUrl: true});
@@ -33,8 +36,25 @@ export class PokemonDetailsComponent implements OnInit {
   ngOnInit() {
   }
 
+  ngAfterViewInit() {
+    setTimeout(() => {
+      const progress = Array.from(document.getElementsByClassName('stat-value') as any);
+      progress.forEach((item: any) => {
+        const max = item.getAttribute('max');
+        this.renderer2.setStyle(item, 'width', item.getAttribute('done') * 100 / max + '%');
+      });
+    }, 200);
+  }
+
   goBack() {
     this.router.navigate(['/tabs']);
+  }
+
+  initializePokemonStats(stats: []) {
+    stats.map((stat: any) => ({name: stat.stat.name, value: stat.base_stat}))
+      .map((data: any) => {
+        this.pokemonStats.push(data);
+      });
   }
 
   getPokemonSpecies(id: string) {
